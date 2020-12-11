0a4f6fdf2e2702c57ca41cfa5aa69c4342f7ba98
Carlo-Bagnoli <carlo.bagnoli.gomez@gmail.com>
2020-12-11 12:32:16 -0400

    Added react-router-dom and created simple routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+// Libs
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 // CSS
 import './App.css';
 
 // Components
 import Home from './pages/Home';
+import About from './pages/About';
 
 function App () {
   return (
-    <div className="App">
-		 <Home />
-    </div>
+     <div className="App">
+        <Router>
+           <Switch>
+				  <Route exact path='/' component={Home} />
+				  <Route path='/about' component={About} />
+           </Switch>
+        </Router>
+     </div>
   );
 }
 
