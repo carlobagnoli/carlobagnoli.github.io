e2245b87ea0dbd7fa291857566a7cca5b4e36815
Carlo-Bagnoli <carlo.bagnoli.gomez@gmail.com>
2020-12-13 17:07:42 -0400

    Added first version of responsive

--- src/pages/Home/components/HomeNavBar/HomeNavBar.tsx
+++ src/pages/Home/components/HomeNavBar/HomeNavBar.tsx
@@ -4,29 +4,46 @@ import {Link} from 'react-router-dom'
 
 // SVG's
 import MainLogo from 'assets/svgs/main_logo_white.svg'
+import MenuIcon from 'assets/svgs/menu_icon.svg'
 
 // CSS
 import './HomeNavBar.css'
 
 function HomeNavBar() {
+	const [windowDimensions, setWindowDimensions] = React.useState({
+		height: window.innerHeight,
+		width: window.innerWidth
+	});
+
+	React.useEffect(() => {
+      const handleChange = () => {
+         setWindowDimensions({
+            height: window.innerHeight,
+            width: window.innerWidth,
+         });
+      };
+
+      window.addEventListener("resize", handleChange);
+
+      return () => window.removeEventListener("resize", handleChange);
+   })
+
 	return (
-      <div className="nav_bar">
-         <Link id="logo" to="/">
-            <img src={MainLogo} alt="main_logo" />
-         </Link>
-         <Link id="link" to="/contact">
-            Contact me
-         </Link>
-         <Link id="link" to="/resume">
-            Résumé
-         </Link>
-         <Link id="link" to="/portfolio">
-            Portfolio
-         </Link>
-         <Link id="link" to="/about">
-            About me
-         </Link>
-      </div>
+      <nav className="nav_bar">
+			<input type="checkbox" id="check" />
+			<label htmlFor="check" className="check_button">
+				<img src={MenuIcon} alt="main_logo" />
+			</label>
+
+			<img className="main_logo" src={MainLogo} alt="main_logo" />
+
+			<ul>
+				<li><h3>Contact</h3></li>
+				<li><h3>Résumé</h3></li>
+				<li><h3>Portfolio</h3></li>
+				<li><h3>About</h3></li>
+			</ul>
+      </nav>
    );
 }
 
