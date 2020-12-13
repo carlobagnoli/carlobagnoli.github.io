a55e51fdfa008e4768fb160d2a6133d0dcc1ca73
Carlo-Bagnoli <carlo.bagnoli.gomez@gmail.com>
2020-12-13 11:39:13 -0400

    Added some animations

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,5 +1,5 @@
 // libs
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 // CSS
 import './HomePage.css'
@@ -15,18 +15,26 @@ import CarloBagnoliPhoto from 'assets/images/carlo_bagnoli_photo.png'
 import BackgroundLogo from 'assets/svgs/background_logo.svg'
 
 function HomePageView () {
+	const [carloPhotoStyle, setCarloPhotoStyle] = useState("main_photo");
+	const [descriptionStyle, setDescriptionStyle] = useState("hide_desc");
+
+	useEffect(() => {
+      setTimeout(() => setDescriptionStyle("show_desc"), 1000);
+   }, []);
+
 	return (
       <div className="home_page">
          <HomeNavBar />
 
-         <h1 id="name">CARLO</h1>
-         <h1 id="lastname">BAGNOLI</h1>
-         <h3 id="mini_description">A medium-rare web dev.</h3>
+			<h1 className={descriptionStyle} id="name">CARLO</h1>
+			<h1 className={descriptionStyle} id="lastname">BAGNOLI</h1>
+			<h3 className={descriptionStyle} id="mini_description">A medium-rare web dev.</h3>
 
          <img id="background_logo" src={BackgroundLogo} alt="background_logo" />
 
          <img
-            id="main_photo"
+            className={carloPhotoStyle}
+            onLoad={() => setCarloPhotoStyle("main_photo show")}
             src={CarloBagnoliPhoto}
             alt="carlo_bagnoli_photo"
          />
